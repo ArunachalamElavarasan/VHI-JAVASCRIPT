fa9b0585c47dec28756214bd9a0c7a5bafbf3039
ArunachalamElavarasan <arunachalamelavarasan31@gmail.com>
2023-06-14 13:09:20 +0530

    All functions are fixed correctly

--- EX-23 Reduce Method/Script files/reduceMethod.js
+++ EX-23 Reduce Method/Script files/reduceMethod.js
@@ -4,7 +4,7 @@
                         *       Developed for          : VHITECH Training Program            *
                         *       Maintenance History                                          *
                         *       Developer              : Arunachalam                         *
-                        *       Creation date          : 13/06/2023      Ticket No:          *
+                        *       Creation date          : 14/06/2023      Ticket No:          *
                         *                                                                   **/
 
 //Date and Time
@@ -15,8 +15,6 @@ document.getElementById('time').innerHTML = dateAndTime.toLocaleTimeString();
 //constant declaration
 let firstSister = [];
 let secondSister = [];
-let firstSisterInput = [];
-let secondSisterInput = [];
 
 //Input details from DOM assigned to variables
 const firstSisFirst = document.getElementById('firstSisFirst');
@@ -30,16 +28,16 @@ const resultField = document.getElementById('resultField');
 const NO_VALUE_ERR = "Please fill all input field to add elements";
 const NO_ELEMENT_ERR = "Please add elements to check result";
 
+//This function is used to add elements as sub array into array if condition is satisfied
 function addItem(){
     if(firstSisFirst.value && firstSisSecond.value && secondSisFirst.value && secondSisSecond.value){
         let arrResult;
-        firstSisterInput.push([parseInt(firstSisFirst.value), parseInt(firstSisSecond.value)]);
-        secondSisterInput.push([parseInt(secondSisFirst.value), parseInt(secondSisSecond.value)]);
-        arrResult = firstSisterInput.join(", ") + "\n";
-        arrResult += secondSisterInput.join(", ");
 
+        firstSister.push([parseInt(firstSisFirst.value), parseInt(firstSisSecond.value)]);
+        secondSister.push([parseInt(secondSisFirst.value), parseInt(secondSisSecond.value)]);
+        arrResult = firstSister.join(", ") + "\n";
+        arrResult += secondSister.join(", ");
         arrElement.value = arrResult;
-
         firstSisFirst.value = firstSisSecond.value = secondSisFirst.value = secondSisSecond.value = "";
     }
     else{
@@ -47,18 +45,39 @@ function addItem(){
     }
 }
 
-function calculate(){
-    if(firstSisterInput.length > 0 && secondSisterInput.length > 0){
+//This function is used to find how much number of items same and show the result to user
+function calculateArea(){
+    if(firstSister.length > 0 && secondSister.length > 0){
+        let firstSisterInput = findArea(firstSister);
+        let secondSisterInput = findArea(secondSister);
+        let matchTotal = 0;
 
+        for(let item = 0; item < firstSisterInput.length; item++){
+            if(firstSisterInput[item] == secondSisterInput[item]) matchTotal += 1;
+        }
+        resultField.value = (matchTotal > 0) ? `${matchTotal} items are matched` : "There is no items are matched"; 
     }
     else{
         alert(NO_ELEMENT_ERR);
     }
 }
 
+//This function is used to return the sub array as area value
+function findArea(findInput){
+    const areaResult = findInput.map(elementInput => {
+        const totalArea = elementInput.reduce((areaValue, currItem) => {
+            areaValue = areaValue * currItem;
+            return areaValue;
+        });
+        return totalArea;
+    });
+    return areaResult;
+}
+
 //This function is used to reset all input fields
 function reset(){
     firstSisFirst.value = firstSisSecond.value = secondSisFirst.value = secondSisSecond.value =
     arrElement.value = resultField.value = "";
-    firstSisterInput = secondSisterInput = [];
+    firstSister = [];
+    secondSister = [];
 }
