94251c3fc05dee7c7b270a5049523d06d3f85ac9
ArunachalamElavarasan <arunachalamelavarasan31@gmail.com>
2023-06-27 11:02:56 +0530

    Function was changed to async and await method

--- EX-28 API Drop Down/Script files/dropDown.js
+++ EX-28 API Drop Down/Script files/dropDown.js
@@ -14,28 +14,35 @@ document.getElementById('time').innerHTML = dateAndTime.toLocaleTimeString();
 
 //variable declaration
 let country;
-const countryDetailsCollection = 'https://restcountries.com/v3.1/all';
 const NO_CAPITAL_ERR = "This country does't have a capital";
 
 //Input details from DOM assigned to variables
 const countryInput = document.getElementById('country');
 const capitalOutput = document.getElementById('capital');
 
-fetch(countryDetailsCollection)                                         //this methos fetch a country details from the api link
-.then(countryCollection => countryCollection.json())
-.then(countryDetails => {
-    country = countryDetails.sort((fitstCountry, secondCountry) => {
-        if(fitstCountry.name.common > secondCountry.name.common) return 1;
-        else if(fitstCountry.name.common < secondCountry.name.common) return -1;
-        else return 0;
-    })
-    country.forEach(countryValue => {
-        const createOption = document.createElement('option');
-        createOption.innerHTML = countryValue.name.common;
-        createOption.setAttribute('value', countryValue.name.common);
-        countryInput.appendChild(createOption);
-    });
-});
+const listCountry = async () => {
+    try {       
+        let countryDetailsCollection = fetch('https://restcountries.com/v3.1/all');
+        countryDetailsCollection = (await countryDetailsCollection).json();
+        country = await countryDetailsCollection;
+
+        country = country.sort((fitstCountry, secondCountry) => {
+            if(fitstCountry.name.common > secondCountry.name.common) return 1;
+            else if(fitstCountry.name.common < secondCountry.name.common) return -1;
+            else return 0;
+        })
+        country.forEach(countryValue => {
+            const createOption = document.createElement('option');
+            createOption.innerHTML = countryValue.name.common;
+            createOption.setAttribute('value', countryValue.name.common);
+            countryInput.appendChild(createOption);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+listCountry();
+
 //this function is used to show a capital when user selects country 
 function showCapital(){
     const countryVal = countryInput.value;
