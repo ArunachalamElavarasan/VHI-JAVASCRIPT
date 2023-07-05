ad3b296323da7479942998dbd6306b029c1152c0
ArunachalamElavarasan <arunachalamelavarasan31@gmail.com>
2023-07-05 11:06:48 +0530

    Card element was stored into function

--- UNO/Script files/uno.js
+++ UNO/Script files/uno.js
@@ -14,7 +14,7 @@ let playerDeck = [];
 const playerContainer = document.getElementById('playerDeck');
 const computerContainer = document.getElementById('computerDeck');
 
-class card{
+class Card{
     constructor(cardColor, cardValue){
         this.color = cardColor;
         this.value = cardValue;
@@ -29,4 +29,34 @@ const randomValue = () => {
     const valueCollection = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "Skip", "Reverse", "Draw"];
     return valueCollection[parseInt(Math.random() * 13)];
 }
+const showCard = cardVal =>`<div class="cardContainer bgLight flexDisplay flexCenter flexItemCenter pointer"><section class="bgGreen card textLight flexDisplay flexBetween flexDirCol">
+                            <section class="flexDisplay"><p class="textLight textBold">${cardVal}</p></section><section class="flexDisplay flexCenter rotateBox">
+                            <section class="bgLight innerCardContainer textGreen flexDisplay flexCenter flexItemCenter"><section class="cardIcon"><p class="cardValue">${cardVal}</p></section></section></section>
+                            <section class="flexDisplay bottomIconRotate"><p class="textLight textBold">${cardVal}</p></section></section></div>`
+
+const hiddenCard = () =>    `<div class="cardContainer bgLight flexDisplay flexCenter flexItemCenter"><section class="bgDark card textLight flexDisplay flexCenter flexDirCol">
+                            <section class="flexDisplay flexCenter flexItemCenter rotateBox posRelative"><section class="bgLight innerCardContainer textGreen flexDisplay flexCenter flexItemCenter flexDirCol">
+                            <section class="flexDisplay colorContainer"><section class="bgBlue smColorContainer"></section><section class="bgRed smColorContainer"></section></section><section class="flexDisplay colorContainer">
+                            <section class="bgGreen smColorContainer"></section><section class="bgYellow smColorContainer"></section></section></section><h1 class="textLight lgFont posAbsolute">4</h1></section></section></div>`;
+
+const beginCard = (addDeck, checkDeck, count) => {
+    if(addDeck.length == count)return 0;
+
+    const newCard = new Card(randomColor(), randomValue());
+    let check = addDeck.concat(checkDeck);
+    const checkAvailable = check.filter(card => card == newCard);
+
+    if(checkAvailable.length < 2){
+        addDeck.push(newCard);
+    }
+    beginCard(addDeck, checkDeck, count);
+}
+
+beginCard(playerDeck, computerDeck, 7);
+beginCard(computerDeck, playerDeck, 7);
+
+
+
+console.table(playerDeck);
+console.table(computerDeck);
 
