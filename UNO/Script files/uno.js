f859acdb3d762bf40f9acee84fd01591b89cd113
ArunachalamElavarasan <arunachalamelavarasan31@gmail.com>
2023-07-05 12:55:01 +0530

    card was added after user clicks card button

--- UNO/Script files/uno.js
+++ UNO/Script files/uno.js
@@ -15,9 +15,11 @@ const playerContainer = document.getElementById('playerDeck');
 const computerContainer = document.getElementById('computerDeck');
 
 class Card{
-    constructor(cardColor, cardValue){
+    constructor(cardColor, cardValue, cardPoint, cardTopIcon){
         this.color = cardColor;
         this.value = cardValue;
+        this.point = cardPoint;
+        this.topIcon = cardTopIcon;
     }
 }
 
@@ -26,13 +28,27 @@ const randomColor = () => {
     return colorCollection[parseInt(Math.random() * 4)];
 }
 const randomValue = () => {
-    const valueCollection = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '<i class="fa-solid fa-rotate"></i>', '<i class="fa-solid fa-ban"></i>', '+2'];
+    const valueCollection = [
+        {value: 0, topIcon: 0, point: 0},
+        {value: 1, topIcon: 1, point: 1},
+        {value: 2, topIcon: 2, point: 2},
+        {value: 3, topIcon: 3, point: 3},
+        {value: 4, topIcon: 4, point: 4},
+        {value: 5, topIcon: 5, point: 5},
+        {value: 6, topIcon: 6, point: 6},
+        {value: 7, topIcon: 7, point: 7},
+        {value: 8, topIcon: 8, point: 8},
+        {value: 9, topIcon: 9, point: 9},
+        {value: '<i class="fa-solid fa-rotate"></i>', topIcon: '<i class="fa-solid fa-rotate"></i>', point: 20},
+        {value: '<i class="fa-solid fa-ban"></i>', topIcon: '<i class="fa-solid fa-ban"></i>', point: 20},
+        {value: '<img src="/Images/twoCard.png" class="cardDraw">', topIcon: '+2', point: 20}
+    ];
     return valueCollection[parseInt(Math.random() * 13)];
 }
-const showCard = (cardVal, cardColor) =>`<div class="cardContainer bgLight flexDisplay flexCenter flexItemCenter pointer"><section class="bg${cardColor} card textLight flexDisplay flexBetween flexDirCol">
-                                        <section class="flexDisplay"><p class="textLight textBold">${cardVal}</p></section><section class="flexDisplay flexCenter rotateBox">
-                                        <section class="bgLight innerCardContainer textGreen flexDisplay flexCenter flexItemCenter"><section class="cardIcon"><p class="cardValue">${cardVal}</p></section></section></section>
-                                        <section class="flexDisplay bottomIconRotate"><p class="textLight textBold">${cardVal}</p></section></section></div>`
+const showCard = (cardVal, cardColor, cardIcon) =>`<div class="cardContainer bgLight flexDisplay flexCenter flexItemCenter pointer"><section class="bg${cardColor} card textLight flexDisplay flexBetween flexDirCol">
+                                        <section class="flexDisplay"><p class="textLight textBold">${cardIcon}</p></section><section class="flexDisplay flexCenter rotateBox">
+                                        <section class="bgLight innerCardContainer text${cardColor} flexDisplay flexCenter flexItemCenter"><section class="cardIcon"><p class="cardValue">${cardVal}</p></section></section></section>
+                                        <section class="flexDisplay bottomIconRotate"><p class="textLight textBold">${cardIcon}</p></section></section></div>`
 
 const hiddenCard = () =>`<div class="cardContainer bgLight flexDisplay flexCenter flexItemCenter"><section class="bgDark card textLight flexDisplay flexCenter flexDirCol">
                         <section class="flexDisplay flexCenter flexItemCenter rotateBox posRelative"><section class="bgLight innerCardContainer textGreen flexDisplay flexCenter flexItemCenter flexDirCol">
@@ -41,8 +57,9 @@ const hiddenCard = () =>`<div class="cardContainer bgLight flexDisplay flexCente
 
 const beginCard = (addDeck, checkDeck, count) => {
     if(addDeck.length == count)return 0;
-
-    const newCard = new Card(randomColor(), randomValue());
+    const cardColor = randomColor()
+    const cardValue = randomValue();
+    const newCard = new Card(cardColor, cardValue.value, cardValue.point, cardValue.topIcon);
     let check = addDeck.concat(checkDeck);
     const checkAvailable = check.filter(card => card == newCard);
 
@@ -52,16 +69,33 @@ const beginCard = (addDeck, checkDeck, count) => {
     beginCard(addDeck, checkDeck, count);
 }
 
+const addCard = (addDeck, checkDeck, cardDeck) => {
+    const cardColor = randomColor();
+    const cardValue = randomValue();
+    const newCard = new Card(cardColor, cardValue.value, cardValue.point, cardValue.topIcon);
+    let check = addDeck.concat(checkDeck);
+    const checkAvailable = check.filter(card => card == newCard);
+
+    if(checkAvailable.length < 2){
+        addDeck.push(newCard);
+        cardDeck.innerHTML += showCard(newCard.value, newCard.color, newCard.topIcon);
+        return 0;
+    }
+    addCard(addDeck, checkDeck);
+}
+
+const insertCard = () => addCard(playerDeck, computerDeck, playerContainer);
+
 beginCard(playerDeck, computerDeck, 7);
 beginCard(computerDeck, playerDeck, 7);
 
 playerDeck.forEach(card => {
-    playerContainer.innerHTML += showCard(card.value, card.color);
+    playerContainer.innerHTML += showCard(card.value, card.color, card.topIcon);
 })
 
 computerDeck.forEach(card => {
-    computerContainer.innerHTML += showCard(card.value, card.color);
-})
+    computerContainer.innerHTML += showCard(card.value, card.color, card.topIcon);
+});
 
 console.table(playerDeck);
-console.table(computerDeck);
+
